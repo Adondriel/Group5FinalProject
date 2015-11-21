abb4db44919aaee387ca9e9c1715212a4e262602
Jason LoBianco <jl9857@ship.edu>
2015-11-21 14:02:02 -0500

    no message

--- src/pokemon/Blastoise.java
+++ src/pokemon/Blastoise.java
@@ -6,25 +6,15 @@ import attacks.*;
  */
 public class Blastoise extends Pokemon
 {
-	int damage=83;
-	int maxHealth=79;
-	int currentHealth=maxHealth;
-	int experience=0;
-	TypeBehavior type=new WaterType();
-	String name="Blastoise";
-	boolean burn=false;
-	boolean freeze=false;
-	boolean poison=false;
-	
-	public int getCurrentHealth(){
-		return currentHealth;
-	}
-	
-	public int getMaxHealth(){
-		return maxHealth;
-	}
-
-	public boolean getPoison() {
-		return poison;
+	public Blastoise(){
+	super.damage=83;
+	super.maxHealth=79;
+	super.currentHealth=maxHealth;
+	super.experience=0;
+	super.type=new WaterType();
+	super.name="Blastoise";
+	super.burn=false;
+	super.freeze=false;
+	super.poison=false;
 	}
 }
