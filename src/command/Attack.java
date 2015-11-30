aaa0d706aac7f0fa473145d7c8562681024a1c43
Warlander362 <bs0805@cs.ship.edu>
2015-11-30 12:24:58 -0500

    Renamed Command Classes

---
+++ src/command/Attack.java
@@ -0,0 +1,18 @@
+package command;
+/**
+ * @author Bradley Solorzano
+ * group 5 final project
+ *
+ */
+public class Attack implements FightCommand
+{
+
+	@Override
+	public void execute()
+	{
+		//get the current pokemon/selected ability and attacks the target
+		//BORDER CASE - only attack once per round.
+		//
+	}
+
+}
---
+++ src/command/Swap.java
@@ -0,0 +1,21 @@
+package command;
+/**
+ * @author Bradley Solorzano
+ * group 5 final project
+ *
+ */
+public class Swap implements SwapCommand
+{
+
+	//creates buttons 
+	@Override
+	public void executeSwap()
+	{
+		//get the current Pokemon on the battlfield
+		//and swaps with the selected
+		//BORDER CASE- cant swap if same Pokemon
+		//BORDER CASE - can't swap a dead pokemon
+		//BORDER CASE - can't swap more then one round
+	}
+
+}
