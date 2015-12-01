c304be3c12cd8edd4d1f43b6ef103d15a217f3f5
Jason LoBianco <jl9857@ship.edu>
2015-12-01 12:32:35 -0500

    Updated the environment to create random pokemon for the computer and to set a random pokemon as the the starting pokemon.

--- src/gameplay/Environment.java
+++ src/gameplay/Environment.java
@@ -1,4 +1,8 @@
 package gameplay;
+import java.util.ArrayList;
+
+
+import pokemon.*;
 /**
  * Provides a battleField for a Player and Computer's Pokemon to do battle in.
  * Author: Jason LoBianco
@@ -12,6 +16,7 @@ public class Environment
 	private static Environment battleField;
 	private Pokemon playerCurrentPokemon;
 	private Pokemon computerCurrentPokemon;
+	private ArrayList<Pokemon> computerPokedex = new ArrayList<Pokemon>();
 	
 	/**
 	 * Creates a battleField for the Player's and Computer's Pokemon to do battle.
@@ -21,9 +26,197 @@ public class Environment
 	private Environment()
 	{
 		player = new Player(null, null);
-		computer = new Computer(null, null);
+		String [] pokemon = {"Blastoise", "Bulbasaur", "Caterpie", "Charizard", "Charmander", "Charmeleon", "Ivysaur", "Poliwag", "Squirtle", "Venusaur", "Vulpix", "Wartortle"};
+		int pokemon1 = (int) (Math.random()*12);
+		String pokemon1String = pokemon[pokemon1];
+		int pokemon2 = (int) (Math.random()*12);
+		String pokemon2String = pokemon[pokemon2];
+		int pokemon3 = (int) (Math.random()*12);
+		String pokemon3String = pokemon[pokemon3];
+		if (pokemon1String == "Blastoise")
+		{
+			Pokemon blastoise = new Blastoise();
+			computerPokedex.add(blastoise);
+		}
+		else if (pokemon1String == "Bulbasaur")
+		{
+			Pokemon bulbasaur = new Bulbasaur();
+			computerPokedex.add(bulbasaur);
+		}
+		else if (pokemon1String == "Caterpie")
+		{
+			Pokemon caterpie = new Caterpie();
+			computerPokedex.add(caterpie);
+		}
+		else if (pokemon1String == "Charizard")
+		{
+			Pokemon charizard = new Charizard();
+			computerPokedex.add(charizard);
+		}
+		else if (pokemon1String == "Charmander")
+		{
+			Pokemon charmander = new Charmander();
+			computerPokedex.add(charmander);
+		}
+		else if (pokemon1String == "Charmeleon")
+		{
+			Pokemon charmeleon = new Charmeleon();
+			computerPokedex.add(charmeleon);
+		}
+		else if (pokemon1String == "Ivysaur")
+		{
+			Pokemon ivysaur = new Ivysaur();
+			computerPokedex.add(ivysaur);
+		}
+		else if (pokemon1String == "Poliwag")
+		{
+			Pokemon poliwag = new Poliwag();
+			computerPokedex.add(poliwag);
+		}
+		else if (pokemon1String == "Squirtle")
+		{
+			Pokemon squirtle = new Squirtle();
+			computerPokedex.add(squirtle);
+		}
+		else if (pokemon1String == "Venusaur")
+		{
+			Pokemon venusaur = new Venusaur();
+			computerPokedex.add(venusaur);
+		}
+		else if (pokemon1String == "Vulpix")
+		{
+			Pokemon vulpix = new Vulpix();
+			computerPokedex.add(vulpix);
+		}
+		else if (pokemon1String == "Wartortle")
+		{
+			Pokemon wartortle = new Wartortle();
+			computerPokedex.add(wartortle);
+		}
+		if (pokemon2String == "Blastoise")
+		{
+			Pokemon blastoise = new Blastoise();
+			computerPokedex.add(blastoise);
+		}
+		else if (pokemon2String == "Bulbasaur")
+		{
+			Pokemon bulbasaur = new Bulbasaur();
+			computerPokedex.add(bulbasaur);
+		}
+		else if (pokemon2String == "Caterpie")
+		{
+			Pokemon caterpie = new Caterpie();
+			computerPokedex.add(caterpie);
+		}
+		else if (pokemon2String == "Charizard")
+		{
+			Pokemon charizard = new Charizard();
+			computerPokedex.add(charizard);
+		}
+		else if (pokemon2String == "Charmander")
+		{
+			Pokemon charmander = new Charmander();
+			computerPokedex.add(charmander);
+		}
+		else if (pokemon2String == "Charmeleon")
+		{
+			Pokemon charmeleon = new Charmeleon();
+			computerPokedex.add(charmeleon);
+		}
+		else if (pokemon2String == "Ivysaur")
+		{
+			Pokemon ivysaur = new Ivysaur();
+			computerPokedex.add(ivysaur);
+		}
+		else if (pokemon2String == "Poliwag")
+		{
+			Pokemon poliwag = new Poliwag();
+			computerPokedex.add(poliwag);
+		}
+		else if (pokemon2String == "Squirtle")
+		{
+			Pokemon squirtle = new Squirtle();
+			computerPokedex.add(squirtle);
+		}
+		else if (pokemon2String == "Venusaur")
+		{
+			Pokemon venusaur = new Venusaur();
+			computerPokedex.add(venusaur);
+		}
+		else if (pokemon2String == "Vulpix")
+		{
+			Pokemon vulpix = new Vulpix();
+			computerPokedex.add(vulpix);
+		}
+		else if (pokemon2String == "Wartortle")
+		{
+			Pokemon wartortle = new Wartortle();
+			computerPokedex.add(wartortle);
+		}
+		if (pokemon3String == "Blastoise")
+		{
+			Pokemon blastoise = new Blastoise();
+			computerPokedex.add(blastoise);
+		}
+		else if (pokemon3String == "Bulbasaur")
+		{
+			Pokemon bulbasaur = new Bulbasaur();
+			computerPokedex.add(bulbasaur);
+		}
+		else if (pokemon3String == "Caterpie")
+		{
+			Pokemon caterpie = new Caterpie();
+			computerPokedex.add(caterpie);
+		}
+		else if (pokemon3String == "Charizard")
+		{
+			Pokemon charizard = new Charizard();
+			computerPokedex.add(charizard);
+		}
+		else if (pokemon3String == "Charmander")
+		{
+			Pokemon charmander = new Charmander();
+			computerPokedex.add(charmander);
+		}
+		else if (pokemon3String == "Charmeleon")
+		{
+			Pokemon charmeleon = new Charmeleon();
+			computerPokedex.add(charmeleon);
+		}
+		else if (pokemon3String == "Ivysaur")
+		{
+			Pokemon ivysaur = new Ivysaur();
+			computerPokedex.add(ivysaur);
+		}
+		else if (pokemon3String == "Poliwag")
+		{
+			Pokemon poliwag = new Poliwag();
+			computerPokedex.add(poliwag);
+		}
+		else if (pokemon3String == "Squirtle")
+		{
+			Pokemon squirtle = new Squirtle();
+			computerPokedex.add(squirtle);
+		}
+		else if (pokemon3String == "Venusaur")
+		{
+			Pokemon venusaur = new Venusaur();
+			computerPokedex.add(venusaur);
+		}
+		else if (pokemon3String == "Vulpix")
+		{
+			Pokemon vulpix = new Vulpix();
+			computerPokedex.add(vulpix);
+		}
+		else if (pokemon3String == "Wartortle")
+		{
+			Pokemon wartortle = new Wartortle();
+			computerPokedex.add(wartortle);
+		}
+		computer = new Computer(computerPokedex, null);
+		computer.setSelectedPokemon(computer.getPokemon((int) (Math.random()*3)));
 		//playerCurrentPokemon = player.getSelectedPokemon();
-		//computerCurrentPokemon = computer.getSelectedPokemon();
+		computerCurrentPokemon = computer.getSelectedPokemon();
 	}
 	
 	/**
